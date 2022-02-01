99f71e2143fa6fdfe58a741e390acef497294402
kbu715 <kbu715@gmail.com>
2022-02-01 22:17:32 +0900

    🚧 Implement toggling sidebar menu

---
+++ js/sidebar.js
@@ -0,0 +1,21 @@
+const sidebarMenuButton = document.querySelector('.gnb-icon-button.is-menu')
+
+const sidebar = document.querySelector('.sidebar')
+
+const sidebarOverlay = document.querySelector('.overlay')
+
+function openSidebar() {
+  // 사이드바, 오버레이가 보이게
+  // .is-active
+  sidebar.classList.add('is-active')
+  sidebarOverlay.classList.add('is-active')
+}
+
+function closeSidebar() {
+  sidebar.classList.remove('is-active')
+  sidebarOverlay.classList.remove('is-active')
+}
+
+sidebarMenuButton.addEventListener('click', openSidebar)
+
+sidebarOverlay.addEventListener('click', closeSidebar)

45c7ee1361937c6af767f68cc68e15106579ef4a
kbu715 <kbu715@gmail.com>
2022-02-07 23:09:53 +0900

    🚧 Implement toggling my-menu

---
+++ js/my-menu.js
@@ -0,0 +1,24 @@
+const myMenu = document.querySelector('.my-menu')
+const myMenuButton = document.querySelector('.my-menu-button')
+
+function closeMyMenuOnClickingOutside(e) {
+  // console.log('WINDOW!!!')
+  // 1. 내가 클릭한 요소가 무엇인지 => e.target
+  // 2. myMenu가 요소를 포함하고 있는지를 알 수 있는 방법 => contains
+  // console.log(myMenu.contains(e.target))
+
+  if (!myMenu.contains(e.target)) {
+    myMenu.classList.remove('is-active')
+    window.removeEventListener('click', closeMyMenuOnClickingOutside)
+  }
+}
+
+function toggleMyMenu() {
+  if (!myMenu.classList.contains('is-active')) {
+    // 앞으로 내가 활성화를 시키겠다!
+    window.addEventListener('click', closeMyMenuOnClickingOutside)
+  }
+  myMenu.classList.toggle('is-active')
+}
+
+myMenuButton.addEventListener('click', toggleMyMenu)

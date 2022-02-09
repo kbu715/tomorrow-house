50e72b3cf96a3ee6a20ca8f985bcfd44cb739e17
kbu715 <kbu715@gmail.com>
2022-02-09 22:32:22 +0900

    🚧 Implement search history using localStorage

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -10,8 +10,32 @@ const gnbSearchHistoryList = gnbSearchHistory.querySelector(
   '.search-history-list'
 )
 
-// console.log(gnbSearchHistoryList)
-// console.log(gnbSearch, gnbSearchInput, gnbSearchHistory)
+let template
+let searchHistoryList
+
+gnbSearchHistoryList.innerHTML = makeSearchHistoryList()
+
+function makeSearchHistoryList() {
+  template = ''
+  searchHistoryList = localStorage.getItem('searchHistoryList')
+    ? localStorage.getItem('searchHistoryList').split(',')
+    : []
+  searchHistoryList.forEach((item) => {
+    template += makeSearchHistoryItem(item)
+  })
+  return template
+}
+
+function makeSearchHistoryItem(item) {
+  return `<li class="search-history-item">
+              <button class="word-button" type="button">
+                ${item}
+              </button>
+              <button class="delete-button" type="button" aria-label="검색어 삭제">
+                <i class="ic-close"></i>
+              </button>
+            </li>`
+}
 
 function closeGnbSearchOnClickingOutside(e) {
   // console.log('WINDOW@@@')
@@ -25,12 +49,27 @@ function openGnbSearchHistory() {
   if (!gnbSearch.classList.contains('is-active')) {
     window.addEventListener('click', closeGnbSearchOnClickingOutside)
   }
+
   gnbSearchHistory.classList.add('is-active')
+  gnbSearchHistoryList.innerHTML = makeSearchHistoryList()
 }
 
 gnbSearchInput.addEventListener('focus', openGnbSearchHistory)
 
+gnbSearchInput.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    if (e.target.value !== '') {
+      searchHistoryList.push(e.target.value)
+      localStorage.setItem('searchHistoryList', searchHistoryList)
+      gnbSearchHistory.classList.remove('is-active')
+      gnbSearchInput.value = ''
+      // elem.blur(): focus를 제거
+      gnbSearchInput.blur()
+    }
+  }
+})
+
 gnbSearchHistoryDeleteAllButton.addEventListener('click', function () {
-  // TODO: LocalStorage로 변경!
-  gnbSearchHistoryList.innerHTML = ''
+  gnbSearchHistory.classList.remove('is-active')
+  localStorage.clear()
 })

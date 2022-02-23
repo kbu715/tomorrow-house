f7f6957da9e02ec90b92aee3457f2ba7bc76ea8e
kbu715 <kbu715@gmail.com>
2022-02-23 22:21:25 +0900

    🚧 Implement toggling product-tab items

---
+++ js/product-tab.js
@@ -0,0 +1,19 @@
+const productTab = document.querySelector('.product-tab')
+const productTabButtonList = productTab.querySelectorAll('button')
+
+let currentActiveTab = productTab.querySelector('.is-active')
+
+function toggleActiveTab() {
+  // 1. is-active
+  const tabItem = this.parentNode
+
+  if (currentActiveTab !== tabItem) {
+    tabItem.classList.add('is-active')
+    currentActiveTab.classList.remove('is-active')
+    currentActiveTab = tabItem
+  }
+}
+
+productTabButtonList.forEach((button) => {
+  button.addEventListener('click', toggleActiveTab)
+})
